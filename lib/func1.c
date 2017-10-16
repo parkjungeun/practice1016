85bfff1f5734c82e9ba1d413cbd240ce3813f43d
Jungeun Park <jungeun3814@naver.com>
2017-10-16 03:22:56 -0700

    Initail commit

---
+++ lib/func1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"func1.h"
+void func1()
+{
+	printf("FUNC1 HELLO!\n");
+}
---
+++ lib/func2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"func2.h"
+
+void func2()
+{
+	printf("FUNC2 HELLO!\n");
+}
